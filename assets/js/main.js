d6bd6e3f36a5878065f6b062298cc16574a7305c
The Kicker <cav.lemasters@geneva.edu>
2022-08-31 12:13:41 -0400

    Restructure page, css changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,28 @@ var p86 = ftc.post86
 var outputOne = document.querySelector(".outputOne")
 for (i=0;i<ori.length;i++)
     {
-        outputOne.innerHTML += `<div class="line"><span class="bubble" style="background-color: ${ori[i].hex}"></span> ${ori[i].color}</div><br>`
+        outputOne.innerHTML += `<div class="card">
+                                    <div class="top">
+                                        <span><span class="bubble" style="background-color:${ori[i].hex}"></span> ${ori[i].color}</span>
+                                    </div>
+                                    <div class="mid">
+                                        Produced: (${ori[i].produced})<br>
+                                        SKU: ${ori[i].sku}
+                                    </div>
+                                </div>
+                                <br>`
     }
 var outputTwo = document.querySelector(".outputTwo")
 for (i=0;i<p86.length;i++)
     {
-        outputTwo.innerHTML += `<div class="line"><span class="bubble" style="background-color: ${p86[i].hex}"></span> ${p86[i].color}</div><br>`
+        outputTwo.innerHTML += `<div class="card">
+                                    <div class="top">
+                                        <span><span class="bubble" style="background-color:${p86[i].hex}"></span> ${p86[i].color}</span>
+                                    </div>
+                                    <div class="mid">
+                                        Produced: (${p86[i].produced})<br>
+                                        SKU: ${p86[i].sku}
+                                    </div>
+                                </div>
+                                <br>`
     }
