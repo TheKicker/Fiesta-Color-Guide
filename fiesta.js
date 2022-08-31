b8450574a7ba463df326f29c5053bda1b5ad1da3
The Kicker <cav.lemasters@geneva.edu>
2022-08-31 10:40:13 -0400

    Added Originals :art:

--- fiesta.js
+++ fiesta.js
@@ -33,7 +33,61 @@ var ftc = {
             "color":"Yellow",
             "sku":"NA",
             "hex":"#DAAC49",
+            "produced": "1936-1969"
+        },
+        {
+            "color":"Old Ivory",
+            "sku":"NA",
+            "hex":"#FFFFF0",
             "produced": "1936-1951"
+        },
+        {
+            "color":"Turquoise",
+            "sku":"NA",
+            "hex":"#56B1BD",
+            "produced": "1937-1969"
+        },
+        {
+            "color":"Forest Green",
+            "sku":"NA",
+            "hex":"#316E15",
+            "produced": "1951-1959"
+        },
+        {
+            "color":"Rose",
+            "sku":"NA",
+            "hex":"#E5908D",
+            "produced": "1951-1959"
+        },
+        {
+            "color":"Chartreuse",
+            "sku":"NA",
+            "hex":"#BCCC6A",
+            "produced": "1951-1959"
+        },
+        {
+            "color":"Gray",
+            "sku":"NA",
+            "hex":"#C4CFDC",
+            "produced": "1951-1959"
+        },
+        {
+            "color":"Medium Green",
+            "sku":"NA",
+            "hex":"#4E9F51",
+            "produced": "1959-1969"
+        },
+        {
+            "color":"Antique Gold",
+            "sku":"NA",
+            "hex":"#FAC752",
+            "produced": "1969-1972"
+        },
+        {
+            "color":"Turf Green",
+            "sku":"NA",
+            "hex":"#7F843C",
+            "produced": "1969-1972"
         }
     ],
     "noProduction":[
