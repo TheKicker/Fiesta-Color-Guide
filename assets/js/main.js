53658d7c8b1d22e821f7918f430d81d461af7c17
The Kicker <cav.lemasters@geneva.edu>
2022-09-01 07:27:27 -0400

    Restructuring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,20 @@
 console.log("Hello world");
 
 // Include Fiesta.js ABOVE this file in DOM
-var ori = ftc.original
-var p86 = ftc.post86
-var all = ori.concat(p86)
+var col = ftc.original
 
-var outputOne = document.querySelector(".outputOne")
-for (i=0;i<ori.length;i++)
+var output = document.querySelector(".output")
+for (i=0;i<col.length;i++)
     {
-        outputOne.innerHTML += `<div class="d-flex">
-                                    <div class="d-l" style="background-color: ${ori[i].hex}"></div>
+        output.innerHTML += `<div class="d-flex">
+                                    <div class="d-l" style="background-color: ${col[i].hex}"></div>
                                     <div class="d-r">
                                         <p class="color">
-                                            ${ori[i].color}
+                                            ${col[i].color}
                                         </p>
                                         <p class="details">
-                                            Produced: ${ori[i].produced} <br>
-                                            SKU: ${ori[i].sku}
-                                        </p>
-                                    </div>
-                                </div>
-                                <br>`
-    }
-var outputTwo = document.querySelector(".outputTwo")
-for (i=0;i<p86.length;i++)
-    {
-        outputTwo.innerHTML += `<div class="d-flex">
-                                    <div class="d-l" style="background-color: ${p86[i].hex}"></div>
-                                    <div class="d-r">
-                                        <p class="color">
-                                            ${p86[i].color}
-                                        </p>
-                                        <p class="details">
-                                            Produced: ${p86[i].produced} <br>
-                                            SKU: ${p86[i].sku}
+                                            Produced: ${col[i].produced} <br>
+                                            SKU: ${col[i].sku}
                                         </p>
                                     </div>
                                 </div>
