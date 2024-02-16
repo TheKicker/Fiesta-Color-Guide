d3e1ed4db1fa32d8590c217c4e00ade3b262270f
The Kicker <cav.lemasters@geneva.edu>
2024-02-16 14:06:03 -0500

    Added Icons and Changed Repository Name

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ var yearFilterLabel = document.getElementById("yearFilterLabel")
 var output = document.getElementById("output")
 
 async function fetchAPI(){
-    let url = "https://thekicker.github.io/FTC-Colors/fiesta.json"
+    let url = "https://thekicker.github.io/Fiesta-Color-Guide/fiesta.json"
     // let url = "fiesta.json"
     try {
         let res = await fetch(url)
